b95e7e480099eaf8d95f6b5670831fe785d04153
Hada Ionut <hadaionut@gmail.com>
2021-08-23 18:34:51 +0300

    individual card

--- Components/SearchItem.js
+++ Components/SearchItem.js
@@ -1,24 +1,47 @@
 import {Card, CardContent, Typography} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
+import {useEffect, useState} from 'react';
+import {Octokit} from '@octokit/rest';
+import Link from 'next/link';
 
 const useStyles = makeStyles({
   root: {
-    width: '20vw',
+    width: '70vw',
     marginTop: '0.5rem',
   },
   text: {
     overflow: 'hidden',
     textOverflow: 'ellipsis'
+  },
+  avatar: {
+    verticalAlign: 'middle',
+    width: '40px',
+    height: '40px',
+    borderRadius: '50%',
+    marginRight: '5px',
+    marginBottom: '5px'
   }
 })
 
 export default function SearchItem({result}) {
   const classes = useStyles();
+  const [bio, setBio] = useState();
+
+
+  useEffect(async () => {
+    const octokit = new Octokit();
+    const results = await octokit.request(`GET /users/${result.login}`, {
+      username: result.login
+    })
+    setBio(results.data.bio);
+  })
 
   return (
       <Card className={classes.root}>
         <CardContent className={classes.root}>
-          <Typography className={classes.text}>{result.login}</Typography>
+          <img src={result.avatar_url} alt="Avatar" className={classes.avatar}/>
+          <span className={classes.text}><Link href={`search/${result.login}`}><a>{result.login}</a></Link></span>
+          <Typography>{bio}</Typography>
         </CardContent>
       </Card>
   )
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,9 +1,9 @@
 import {Button} from '@material-ui/core';
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 import {useSelector} from 'react-redux';
 import {useEffect, useState} from 'react';
 import {Octokit} from '@octokit/rest';
-import SearchItem from '../Components/SearchItem';
+import SearchItem from '../../Components/SearchItem';
 
 export default function Search() {
   const searchValue = useSelector(state => state.searchQuerry) || 'abc';
