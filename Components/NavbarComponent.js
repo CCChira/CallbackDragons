d82a243c6fc01b581d3d7bb4e5fbe9c300fef313
Ciceu Tiberiu <tiberiu.ciceu@evozon.com>
2021-08-24 13:24:24 +0300

    added login

--- Components/NavbarComponent.js
+++ Components/NavbarComponent.js
@@ -1,41 +1,50 @@
 import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, IconButton, makeStyles, Typography , Badge, } from '@material-ui/core';
-import { AccountTreeSharp, FaceSharp } from '@material-ui/icons';
+import {Toolbar, IconButton, makeStyles, Typography, Badge,} from '@material-ui/core';
+import {useUser} from "@auth0/nextjs-auth0";
+import {AccountTreeSharp, FaceSharp} from '@material-ui/icons';
+
 const useStyles = makeStyles({
-  navbar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    background: "rgb(135,200,255)",
-    background: "radial-gradient(circle, rgba(135,200,255,0.9669001389618347) 43%, rgba(141,214,255,1) 75%, rgba(102,209,255,1) 100%)"
-  },
-  logo: {
-    position: 'relative',
-    color: '#FFFFFF',
-    padding: '2px',
-  },
-  toolbar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  profile: {
-    position: 'relative',
-    color: 'white',
-  }
+    navbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        background: "rgb(135,200,255)",
+        background: "radial-gradient(circle, rgba(135,200,255,0.9669001389618347) 43%, rgba(141,214,255,1) 75%, rgba(102,209,255,1) 100%)"
+    },
+    logo: {
+        position: 'relative',
+        color: '#FFFFFF',
+        padding: '2px',
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    profile: {
+        position: 'relative',
+        color: 'white',
+    }
 })
+
 function NavbarComponent() {
-  const classes = useStyles();
-  return (
-    <div>
-      <AppBar position = 'static' className = {classes.navbar}>
-        <Toolbar className = {classes.toolbar}>
-            <AccountTreeSharp fontSize = "large" className = {classes.logo}/>
-            <Typography className = {classes.logo} variant= "h5">CBDragons</Typography>
-            <FaceSharp className = {classes.profile}/>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
+    const {user} = useUser();
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBar position='static' className={classes.navbar}>
+                <Toolbar className={classes.toolbar}>
+                    <AccountTreeSharp fontSize="large" className={classes.logo}/>
+                    <Typography className={classes.logo} variant="h5">CBDragons</Typography>
+                    {user ? <a href="/api/auth/logout">Welcome {user.name}. Logout</a>
+                        :
+                        <a href="/api/auth/login">
+                            <FaceSharp className={classes.profile}/>
+                        </a>
+                    }
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 }
 
 export default NavbarComponent
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
 import styles from '../styles/Home.module.css';
 import {useDispatch, useSelector} from 'react-redux';
 import {decrementCount, incrementCount, resetCount} from '../store/actions';
+import {useUser} from "@auth0/nextjs-auth0";
 import Link from 'next/link';
 
 export default function Home() {
-  const count = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+    const {user, error, isLoading} = useUser();
+    const count = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+    console.log(user);
 
-  return (
-      <>
+    if (isLoading) return <div>Loading...</div>
+
+    if (error) return <div>{error.message}</div>
+
+    return (
         <div className={styles.container}>
-          <div>
-            <h1>
-              Count: <span>{count}</span>
-            </h1>
-            <button onClick={() => dispatch(incrementCount())}>+1</button>
-            <button onClick={() => dispatch(decrementCount())}>-1</button>
-            <button onClick={() => dispatch(resetCount())}>Reset</button>
-          </div>
-          <Link href="/otherPage">
-            <a>Other Page</a>
-          </Link>
+            <div>
+                <h1>
+                    Count: <span>{count}</span>
+                </h1>
+                <button onClick={() => dispatch(incrementCount())}>+1</button>
+                <button onClick={() => dispatch(decrementCount())}>-1</button>
+                <button onClick={() => dispatch(resetCount())}>Reset</button>
+            </div>
+            <Link href="/otherPage">
+                <a>Other Page</a>
+            </Link>
         </div>
-      </>
-  );
+    )
 }
+
+
