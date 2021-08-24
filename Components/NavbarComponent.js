dbf3769298537fc5ffb829065d7b1c35c467e362
chira-cristian <chira.cristian621@gmail.com>
2021-08-24 16:23:56 +0300

    small fixes

--- Components/NavbarComponent.js
+++ Components/NavbarComponent.js
@@ -1,12 +1,19 @@
 import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, IconButton, makeStyles, Typography , Badge, } from '@material-ui/core';
+import {
+  Toolbar,
+  IconButton,
+  makeStyles,
+  Typography,
+  Badge,
+} from '@material-ui/core';
 import { AccountTreeSharp, FaceSharp } from '@material-ui/icons';
 const useStyles = makeStyles({
   navbar: {
     display: 'flex',
     justifyContent: 'space-between',
-    background: "rgb(135,200,255)",
-    background: "radial-gradient(circle, rgba(135,200,255,0.9669001389618347) 43%, rgba(141,214,255,1) 75%, rgba(102,209,255,1) 100%)"
+    background: 'rgb(135,200,255)',
+    background:
+      'radial-gradient(circle, rgba(135,200,255,0.9669001389618347) 43%, rgba(141,214,255,1) 75%, rgba(102,209,255,1) 100%)',
   },
   logo: {
     display: 'flex',
@@ -22,23 +29,25 @@ const useStyles = makeStyles({
   profile: {
     position: 'relative',
     color: 'white',
-  }
-})
+  },
+});
 function NavbarComponent() {
   const classes = useStyles();
   return (
     <div>
-      <AppBar position = 'static' className = {classes.navbar}>
-        <Toolbar className = {classes.toolbar}>
-          <div className = {classes.logo}>
-            <AccountTreeSharp fontSize = "large" className = {classes.logo}/>
-            <Typography className = {classes.logo} variant= "h5">CBDragons</Typography>
+      <AppBar position='static' className={classes.navbar}>
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.logo}>
+            <AccountTreeSharp fontSize='large' className={classes.logo} />
+            <Typography className={classes.logo} variant='h5'>
+              CBDragons
+            </Typography>
           </div>
-            <FaceSharp className = {classes.profile}/>
+          <FaceSharp className={classes.profile} />
         </Toolbar>
       </AppBar>
     </div>
-  )
+  );
 }
 
-export default NavbarComponent
+export default NavbarComponent;
--- Components/RepoViewComponent.js
+++ Components/RepoViewComponent.js
@@ -1,26 +1,27 @@
-import { Card, makeStyles, Typography } from "@material-ui/core"
-import {DescriptionTwoTone, FolderOpenTwoTone} from '@material-ui/icons'
+import { Card, makeStyles, Typography } from '@material-ui/core';
+import { DescriptionTwoTone, FolderOpenTwoTone } from '@material-ui/icons';
 const useStyles = makeStyles({
   fileCard: {
-    display: 'flex', 
+    display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    width: '80vw',
+    width: '100%',
     height: '5vh',
-  }
-})
-function RepoViewComponent({fileName, fileType}) {
+  },
+});
+function RepoViewComponent({ fileName, fileType }) {
   const classes = useStyles();
   return (
     <div>
-        <div className = {classes.fileCard}>
-          {(fileType === "blob") ? <DescriptionTwoTone color ="primary"/> : <FolderOpenTwoTone color = "primary"/>}
-          <Typography variant = "h5">
-            {fileName}
-          </Typography>
-        </div>
+      <div className={classes.fileCard}>
+        {fileType === 'blob' ? (
+          <DescriptionTwoTone color='primary' />
+        ) : (
+          <FolderOpenTwoTone color='primary' />
+        )}
+        <Typography variant='h6'>{fileName}</Typography>
+      </div>
     </div>
-  )
+  );
 }
-
-export default RepoViewComponent
+export default RepoViewComponent;
