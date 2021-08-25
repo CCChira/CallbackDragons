61a0a6bc4cad4893238e85c491b82e2a47a9d366
Hrothgar32 <almos.zediu@evozon.com>
2021-08-25 12:29:21 +0300

    Refractored fileView

--- pages/search/[userName]/[repoName]/[blobName].js
+++ pages/search/[userName]/[repoName]/[blobName].js
@@ -1,3 +1,28 @@
-export default function BlobName () {
-  return (<div/>)
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import {Octokit} from '@octokit/rest';
+import Highlight from 'react-highlight';
+
+const BASE_URL='https://api.github.com/repos/CCChira/FQuentFinal/git/blobs/'
+
+export default function FileViewer(){
+  const router = useRouter();
+  const { userName, repoName, blobName } = router.query;
+  const [fileContent, setFileContent] = useState('');
+
+  useEffect(async () => {
+    if(router.isReady){
+        const octokit = new Octokit();
+        const url = `https://api.github.com/repos/${userName}/${repoName}/git/blobs/${blobName}`;
+        const res = await octokit.request(`GET ${url}`);
+        console.log(res);
+        setFileContent(Buffer.from(res.data.content, 'base64').toString());
+    }
+  },[router.isReady])
+
+  return(
+    <Highlight>
+      { fileContent }
+    </Highlight>
+  )
 }
