1d6e79ceaf54c8d290433e9c47edfb19b6410b64
Hada Ionut <hadaionut@gmail.com>
2021-08-26 13:03:24 +0300

    Added Tibi's most used languages

---
+++ Components/MostUsedByUser.js
@@ -0,0 +1,51 @@
+import {Typography} from '@material-ui/core';
+import React from 'react';
+
+export default function MostUsedByUser({classes, repos}) {
+  let languages = repos?.reduce?.((total, item) => {
+    const { language } = item;
+    console.log(language);
+
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 3);
+
+  let sum = 0;
+  for (let i = 0; i < languages.length; ++i) {
+    sum += languages[i].value;
+  }
+
+  for (let i = 0; i < languages.length; ++i) {
+    languages[i].value = ((languages[i].value * 100) / sum).toFixed(2);
+  }
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingBottom: '5px'}}>
+      <Typography className={classes.userName} style={{marginRight: '10px'}}>
+        Most used languages:
+      </Typography>
+        {languages.map((language, index) => {
+          return (
+            <Typography key={index} style={{marginRight: '10px'}}>
+              {language.label}: {language.value}%
+            </Typography>
+          );
+        })}
+    </div>
+  );
+}
