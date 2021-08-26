d13b97a8e45f47f0d5938b9013e8c7f0c5eb23ec
Hada Ionut <hadaionut@gmail.com>
2021-08-26 11:55:13 +0300

    added file name to fileView

--- pages/search/[userName]/[repoName]/[blobName].js
+++ pages/search/[userName]/[repoName]/[blobName].js
@@ -1,16 +1,17 @@
 import {useRouter} from 'next/router';
-import {Card, CardContent, makeStyles} from '@material-ui/core';
+import {Button, Card, CardActions, CardContent, makeStyles, Typography} from '@material-ui/core';
 import {useState, useEffect} from 'react';
 import {Octokit} from '@octokit/rest';
 import Highlight from 'react-highlight';
 import styles from '../../../../styles/Home.module.css';
+import {useSelector} from 'react-redux';
+import Link from '@material-ui/core/Link';
 
 const useStyle = makeStyles({
   hljs: {
     display: 'inline-block',
     overflowX: 'auto',
     overflowY: 'auto',
-    padding: '0.5em',
     height: '60vh',
     width: '80vw',
   }
@@ -21,33 +22,39 @@ export default function FileViewer() {
   const router = useRouter();
   const {userName, repoName, blobName} = router.query;
   const [fileContent, setFileContent] = useState('');
+  const historyStack = useSelector(state => state.fileLocation);
+  const [fileName, setFileName] = useState('');
 
   useEffect(async () => {
     if (router.isReady) {
       const octokit = new Octokit();
       const url = `https://api.github.com/repos/${userName}/${repoName}/git/blobs/${blobName}`;
       const res = await octokit.request(`GET ${url}`);
-      const repo = await octokit.request(
-        `GET /repos/${userName}/${repoName}/commits`
-      );
-      const repoContents = await octokit.request(
-        `GET /repos/${userName}/${repoName}/commits/${repo.data[0]['sha']}`
-      );
-      const files = await octokit.request(`GET ${repoContents?.data.commit.tree.url}`);
-      console.log(files.data.tree);
+
       setFileContent(Buffer.from(res.data.content, 'base64').toString());
     }
   }, [router.isReady]);
 
+  useEffect(async () => {
+    const target = historyStack[historyStack.length - 1];
+    if (!target) return;
+    const octokit = new Octokit();
+    const res = await octokit.request(`GET ${historyStack[historyStack.length - 1]}`)
+    setFileName(res?.data?.tree?.filter?.(item => item.sha === blobName)?.[0]?.path);
+  }, [historyStack])
+
   return (
     <div className={styles.container}>
-      <Card>
-        <CardContent>
-          ceapa
+      <Card style={{marginTop: '1.5 rem'}}>
+        <CardContent style={{textAlign: 'center'}}>
+          <Typography variant='h4'>{fileName}</Typography>
         </CardContent>
+        <CardActions>
+          <Link style={{textDecoration: 'none', color: 'black'}} href={`/search/${userName}/${repoName}`}><Button variant="contained" color="primary">Back to the repo</Button></Link>
+        </CardActions>
       </Card>
 
-      <Highlight className={classes.hljs}>
+      <Highlight style={{margin: 0}} className={classes.hljs}>
         {fileContent}
       </Highlight>
     </div>
