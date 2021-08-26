12d817db17577c11e5852ff6cc65b6986def56fa
Hada Ionut <hadaionut@gmail.com>
2021-08-26 10:19:35 +0300

    slightly reduced padding

--- pages/search/[userName]/[repoName]/index.js
+++ pages/search/[userName]/[repoName]/index.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles({
     minHeight: '10px',
     overflow: 'hidden',
     minWidth: '150px',
-    margin: '2rem 0'
+    margin: '1.5rem 0'
   },
   userAvatar: {
     verticalAlign: 'middle',
@@ -28,7 +28,7 @@ const useStyles = makeStyles({
     marginBottom: '5px',
   },
   repoPaper: {
-    marginBottom: '2rem',
+    marginBottom: '1.5rem',
     maxWidth: '1000px',
     maxHeight: '50%',
     width: '75%',
